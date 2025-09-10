cb8a1e57605806197c177397cddc26e4d72d8a3b
ThiagoQSS <tqss2759@gmail.com>
2025-09-09 21:43:46 -0300

    Código completo com testes

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -84,24 +84,23 @@ class AbrigoAnimais {
       }
       encontrados.push(item);
     }
-    return false;
   }
 
   static resetResultado() {
     AbrigoAnimais.resultado = { lista: [], erro: '' };
   }
 
-  //Solução deve estar aqui, mas posso criar metodos auxiliares
-  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    AbrigoAnimais.resetResultado();
-
-    const brinquedosPessoa1Array = brinquedosPessoa1.split(',');
-    const brinquedosPessoa2Array = brinquedosPessoa2.split(',');
-    const ordemAnimaisArray = ordemAnimais.split(',');
-
-    const animaisPessoa1 = [];
-    const animaisPessoa2 = [];
-
+  /**
+   * Checa os erros de acordo com os parâmetros
+   * @param {array} brinquedosPessoa1Array
+   * @param {array} brinquedosPessoa2Array
+   * @param {array} ordemAnimaisArray
+   */
+  static buscarErros(
+    brinquedosPessoa1Array,
+    brinquedosPessoa2Array,
+    ordemAnimaisArray
+  ) {
     for (const animal of ordemAnimaisArray) {
       AbrigoAnimais.checarAnimal(animal);
     }
@@ -115,15 +114,177 @@ class AbrigoAnimais {
     AbrigoAnimais.checarDuplicatas(brinquedosPessoa1Array, 'Brinquedo');
     AbrigoAnimais.checarDuplicatas(brinquedosPessoa2Array, 'Brinquedo');
     AbrigoAnimais.checarDuplicatas(ordemAnimaisArray, 'Animal');
+  }
+
+  /**
+   * Entrega o animal à pessoa, se possível.
+   * Para que seja possível, a pessoa deve ter menos de 3 animais.
+   * Se o animal for um gato, checamos os brinquedos dos animais já adotados
+   * Se ela já tiver um gato, o animal n podera usar seus brinquedos.
+   * @param {string} animal
+   * @param {array} animaisPessoaArray
+   * @returns
+   */
+  static entregarAnimal(animal, animaisPessoaArray) {
+    // Checando se a pessoa ja tem 3 animais
+    if (animaisPessoaArray.length >= 3) {
+      return false;
+    }
+    // Checando se algum animal da pessoa quer brinquedos do gato
+    const infoAnimal = AbrigoAnimais.animais.find((a) => a.nome === animal);
+    if (infoAnimal.tipo == 'gato') {
+      for (const adotado of animaisPessoaArray) {
+        const brinquedosAdotado = AbrigoAnimais.animais.find(
+          (a) => a.nome === adotado
+        ).brinquedos;
+        for (const brinquedo of infoAnimal.brinquedos) {
+          if (brinquedosAdotado.includes(brinquedo)) {
+            return false;
+          }
+        }
+      }
+    }
+    // Checando se a pessoa tem gatos
+    for (const adotado of animaisPessoaArray) {
+      const infoAdotado = AbrigoAnimais.animais.find((a) => a.nome === adotado);
+      if (infoAdotado.tipo == 'gato') {
+        const brinquedosAdotado = infoAdotado.brinquedos;
+        for (const brinquedo of infoAnimal.brinquedos) {
+          if (brinquedosAdotado.includes(brinquedo)) {
+            return false;
+          }
+        }
+      }
+    }
+
+    animaisPessoaArray.push(animal);
+    return true;
+  }
+
+  //Solução deve estar aqui, mas posso criar metodos auxiliares
+  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    AbrigoAnimais.resetResultado();
+
+    // Check de erros
+    const brinquedosPessoa1Array = brinquedosPessoa1.split(',');
+    const brinquedosPessoa2Array = brinquedosPessoa2.split(',');
+    const ordemAnimaisArray = ordemAnimais.split(',');
+
+    AbrigoAnimais.buscarErros(
+      brinquedosPessoa1Array,
+      brinquedosPessoa2Array,
+      ordemAnimaisArray
+    );
 
     if (AbrigoAnimais.resultado.erro !== '') {
       AbrigoAnimais.resultado.lista = false;
       return AbrigoAnimais.resultado;
     }
 
-    // Código da solução vem aqui...
+    // Código da solução vem aqui
+    const animaisPessoa1 = [];
+    const animaisPessoa2 = [];
+
+    for (const animal of ordemAnimaisArray) {
+      const infoAnimal = AbrigoAnimais.animais.find((a) => a.nome === animal);
+
+      const brinquedosDesejados = infoAnimal.brinquedos;
+
+      let aptidaoPessoa1;
+      let aptidaoPessoa2;
+
+      if (infoAnimal.nome == 'Loco') {
+        if (animaisPessoa1.length > 0)
+          aptidaoPessoa1 = AbrigoUtils.checarAptidaoSemOrdem(
+            brinquedosDesejados,
+            brinquedosPessoa1Array
+          );
+        else
+          aptidaoPessoa1 = AbrigoUtils.checarAptidaoComOrdem(
+            brinquedosDesejados,
+            brinquedosPessoa1Array
+          );
+        // console.log("Pessoa1: ", aptidaoPessoa1);
+        if (animaisPessoa2.length > 0)
+          aptidaoPessoa2 = AbrigoUtils.checarAptidaoSemOrdem(
+            brinquedosDesejados,
+            brinquedosPessoa2Array
+          );
+        else
+          aptidaoPessoa2 = AbrigoUtils.checarAptidaoComOrdem(
+            brinquedosDesejados,
+            brinquedosPessoa2Array
+          );
+        // console.log("Pessoa2: ", aptidaoPessoa2);
+      } else {
+        aptidaoPessoa1 = AbrigoUtils.checarAptidaoComOrdem(
+          brinquedosDesejados,
+          brinquedosPessoa1Array
+        );
+        // console.log("Pessoa1: ", aptidaoPessoa1);
+        aptidaoPessoa2 = AbrigoUtils.checarAptidaoComOrdem(
+          brinquedosDesejados,
+          brinquedosPessoa2Array
+        );
+        // console.log("Pessoa2: ", aptidaoPessoa2);
+      }
+      if (aptidaoPessoa1 && aptidaoPessoa2) {
+        AbrigoAnimais.resultado.lista.push(`${animal} - abrigo`); // (tadinho)
+      } else if (aptidaoPessoa1) {
+        if (AbrigoAnimais.entregarAnimal(animal, animaisPessoa1)) {
+          AbrigoAnimais.resultado.lista.push(`${animal} - pessoa 1`);
+        } else {
+          AbrigoAnimais.resultado.lista.push(`${animal} - abrigo`);
+        }
+      } else if (aptidaoPessoa2) {
+        if (AbrigoAnimais.entregarAnimal(animal, animaisPessoa2)) {
+          AbrigoAnimais.resultado.lista.push(`${animal} - pessoa 2`);
+        } else {
+          AbrigoAnimais.resultado.lista.push(`${animal} - abrigo`);
+        }
+      } else {
+        AbrigoAnimais.resultado.lista.push(`${animal} - abrigo`);
+      }
+    }
+
+    AbrigoAnimais.resultado.lista.sort();
 
     return AbrigoAnimais.resultado;
+  }
+}
+
+class AbrigoUtils {
+  static checarAptidaoComOrdem(
+    brinquedosDesejadosArray,
+    brinquedosPessoaArray
+  ) {
+    // Funcionando
+    // console.log("Desejados: ", brinquedosDesejadosArray, "\nPessoa: ", brinquedosPessoaArray);
+    let brinquedosRestantes = [...brinquedosPessoaArray];
+    let aptidao = true;
+    for (const brinquedo of brinquedosDesejadosArray) {
+      if (brinquedosRestantes.includes(brinquedo)) {
+        brinquedosRestantes = brinquedosRestantes.slice(
+          brinquedosRestantes.indexOf(brinquedo)
+        );
+      } else {
+        aptidao = false;
+      }
+    }
+    return aptidao;
+  }
+
+  static checarAptidaoSemOrdem(
+    brinquedosDesejadosArray,
+    brinquedosPessoaArray
+  ) {
+    let aptidao = true;
+    for (const brinquedo of brinquedosDesejadosArray) {
+      if (!brinquedosPessoaArray.includes(brinquedo)) {
+        aptidao = false;
+      }
+    }
+    return aptidao;
   }
 }
 
--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -1,6 +1,8 @@
 import { AbrigoAnimais } from './abrigo-animais';
 
 describe('Abrigo de Animais', () => {
+  // Testes de erro de entrada =======================================================================================================
+  
   test('Deve rejeitar animal inválido', () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
       'CAIXA,RATO',
@@ -9,7 +11,39 @@ describe('Abrigo de Animais', () => {
     );
     expect(resultado.erro).toBe('Animal inválido');
     expect(resultado.lista).toBeFalsy();
-  });
+  }); // Correto
+
+  test('Deve rejeitar brinquedo inválido', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'CAIXA,RATO',
+      'RATO,BRINQUEDOFALSO',
+      'Loco'
+    );
+    expect(resultado.erro).toBe('Brinquedo inválido');
+    expect(resultado.lista).toBeFalsy();
+  }); // Correto
+
+  test('Deve rejeitar animal duplicado', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'CAIXA,RATO',
+      'RATO,CAIXA',
+      'Loco, Rex, Loco'
+    );
+    expect(resultado.erro).toBe('Animal inválido');
+    expect(resultado.lista).toBeFalsy();
+  }); // Correto
+
+  test('Deve rejeitar brinquedo duplicado', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'CAIXA,RATO',
+      'RATO,CAIXA,RATO',
+      'Loco, Rex, Mimi'
+    );
+    expect(resultado.erro).toBe('Brinquedo inválido');
+    expect(resultado.lista).toBeFalsy();
+  }); // Correto
+  
+  // Testes de funcionamento do código de encontrar animais ==========================================================================
 
   test('Deve encontrar pessoa para um animal', () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
@@ -21,7 +55,7 @@ describe('Abrigo de Animais', () => {
     expect(resultado.lista[1]).toBe('Rex - pessoa 1');
     expect(resultado.lista.length).toBe(2);
     expect(resultado.erro).toBeFalsy();
-  });
+  }); 
 
   test('Deve encontrar pessoa para um animal intercalando brinquedos', () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
@@ -36,5 +70,68 @@ describe('Abrigo de Animais', () => {
     expect(resultado.lista[3]).toBe('Rex - abrigo');
     expect(resultado.lista.length).toBe(4);
     expect(resultado.erro).toBeFalsy();
-  });
+  }); // Correto
+
+  test('Deve limitar quantidade de animais por pessoa', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'BOLA',
+      'LASER,SKATE,RATO,BOLA,CAIXA,NOVELO',
+      'Bebe,Bola,Loco,Rex'
+    );
+
+    expect(resultado.lista[0]).toBe('Bebe - pessoa 2');
+    expect(resultado.lista[1]).toBe('Bola - pessoa 2');
+    expect(resultado.lista[2]).toBe('Loco - pessoa 2');
+    expect(resultado.lista[3]).toBe('Rex - abrigo');
+    expect(resultado.lista.length).toBe(4);
+    expect(resultado.erro).toBeFalsy();
+  }); // Correto
+
+  test('Deve recusar a entrega do gato quando outro animal quiser o brinquedo', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'LASER,RATO,BOLA',
+      'BOLA',
+      'Bebe,Zero'
+    );
+    expect(resultado.lista[0]).toBe('Bebe - pessoa 1');
+    expect(resultado.lista[1]).toBe('Zero - abrigo');
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
+  }) // Correto
+
+  test('Deve recusar a entrega do animal quando um gato já usa o brinquedo', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'RATO,BOLA',
+      'BOLA',
+      'Zero,Rex'
+    );
+    expect(resultado.lista[0]).toBe('Rex - abrigo');
+    expect(resultado.lista[1]).toBe('Zero - pessoa 1');
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
+  }) // Correto
+
+  test('Deve aceitar loco mesmo com a ordem errada quando já houver algum animal de companhia', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'RATO,BOLA,SKATE',
+      'BOLA',
+      'Rex,Loco'
+    );
+    expect(resultado.lista[0]).toBe('Loco - pessoa 1');
+    expect(resultado.lista[1]).toBe('Rex - pessoa 1');
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
+  }) // Correto
+
+  test('Deve negar loco quando a ordem estiver errada e não houver algum animal de companhia', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'RATO,SKATE',
+      'BOLA',
+      'Zero,Loco'
+    );
+    expect(resultado.lista[0]).toBe('Loco - abrigo');
+    expect(resultado.lista[1]).toBe('Zero - abrigo');
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
+  }) // Correto
 });
